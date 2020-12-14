3f0179ea4888703b6f6e3117702fbdfecad34042
Pancor <pancordev@gmail.com>
2020-12-14 12:36:27 +0100

    Removed author.

--- src/main/java/pl/pancordev/leak/SecurityConfig.java
+++ src/main/java/pl/pancordev/leak/SecurityConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * @author Mateusz Becker
- */
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
